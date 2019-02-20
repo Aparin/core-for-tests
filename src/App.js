ebf4a7f1ba8cf1ae34910207b45fd87d3e9e37a2
Апарин Александр <mir-uspeha@mail.ru>
2019-02-20 10:56:26 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './animate.css';
 
 class App extends Component {
+  state = {
+    questionNumber: 1,
+    key: 1,
+    result: 0,
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App animated fadeInDown">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
